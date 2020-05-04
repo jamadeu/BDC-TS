87f48e1fd18cea8dbd92e429509ca5b930d228d4
Jean Amadeu <jean.amadeu2@gmail.com>
2020-05-03 21:06:20 -0300

    Feat: List requests
    Create route to list all requests

--- src/routes/request.routes.ts
+++ src/routes/request.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
 
 import CreateRequestService from '../services/CreateRequestService';
 import UpdateRequestService from '../services/UpdateRequestService';
+import Request from '../models/Request';
 
 const requestRouter = Router();
 
@@ -28,6 +30,12 @@ requestRouter.put('/:id', async (request, response) => {
   const requestUpdated = await updateRequest.execute({ id, ...request.body });
 
   return response.json(requestUpdated);
+});
+
+requestRouter.get('/', async (request, response) => {
+  const requests = await getRepository(Request).find();
+
+  return response.json(requests);
 });
 
 export default requestRouter;
